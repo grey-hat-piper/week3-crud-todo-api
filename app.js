a428f1864de9991f021d4f1170fbcd5a96bad999
princederek <marteyderek08@gmail.com>
2025-11-28 10:14:46 +0000

    submit

--- app.js
+++ app.js
@@ -12,9 +12,32 @@ app.get('/todos', (req, res) => {
   res.status(200).json(todos); // Send array as JSON
 });
 
+// GET completed - read
+app.get('/todos/completed', (req, res) => {
+  const completed = todos.filter((t) => t.completed);
+  res.status(200).json(completed); // Custom Read!
+});
+
+// GET not completed - read
+app.get('/todos/active', (req, res) => {
+  const active = todos.filter((t) => t.completed !== true);
+  res.json(active); // Custom Read!
+});
+
+// GET single - Read
+app.get('/todos/:id', (req, res) => {
+  const todo = todos.find((t) => t.id === parseInt(req.params.id)); // Array.find()
+  if (!todo) return res.status(404).json({ message: 'Todo not found' });
+  res.status(200).json(todo);
+})
+
+
 // POST New – Create
 app.post('/todos', (req, res) => {
   const newTodo = { id: todos.length + 1, ...req.body }; // Auto-ID
+  // Task Validation
+  const task = newTodo.task;
+  if (!task) return res.status(404).json({ message: 'Task required' });
   todos.push(newTodo);
   res.status(201).json(newTodo); // Echo back
 });
@@ -35,11 +58,6 @@ app.delete('/todos/:id', (req, res) => {
   if (todos.length === initialLength)
     return res.status(404).json({ error: 'Not found' });
   res.status(204).send(); // Silent success
-});
-
-app.get('/todos/completed', (req, res) => {
-  const completed = todos.filter((t) => t.completed);
-  res.json(completed); // Custom Read!
 });
 
 app.use((err, req, res, next) => {
